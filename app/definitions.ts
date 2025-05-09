0c4be4e64e7c2c49128f59ad098f9d9a4538384d
umfb <abdulbasitadebajo1619@gmail.com>
2025-05-09 03:54:00 +0100

    adjusted type

--- app/definitions.ts
+++ app/definitions.ts
@@ -1,7 +1,11 @@
-import { ComponentType } from "react";
+import { ComponentType, ReactNode } from "react";
 
 export type FeatureCardPropsType = {
   title: string;
   description: string;
-  Icon: ComponentType<any>;
+  Icon: ComponentType<{
+    size?: number | string;
+    color?: string;
+    className?: string;
+  }>;
 };
